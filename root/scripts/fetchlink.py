4387dbfc4f879da0e06d80407732c9530667cbed
Witherking25 <39203202+Witherking25@users.noreply.github.com>
2020-10-31 23:21:33 -0500

    Update fetchlink.py

--- root/scripts/fetchlink.py
+++ root/scripts/fetchlink.py
@@ -1,8 +1,10 @@
 import requests
 import json
+import subprocess
+from discord_webhook import DiscordWebhook
 
-url = 'https://api.jsonbin.io/b/5f9dc3253269193b17bfec93'
-headers = {'secret-key': ''}
+url = 'https://api.jsonbin.io/b/5f9dc3253269193b17bfec93/latest'
+headers = {'secret-key': '$2b$10$ihoA/Dnxdha4mErdATrtQe8HF5nPG44ZKDL2ZVheKgbHeL9aW./QG'}
 
 req = requests.get(url, headers=headers)
 t = req.text
@@ -10,11 +12,13 @@ y = json.loads(t)
 
 response = requests.get(y['imageurl'])
 
-file = open("betternamethansample_image.png", "wb")
+file = open("image.png", "wb")
 file.write(response.content)
 file.close()
-result = subprocess.run(['qrscanner',"betternamethansample_image.png" ], stdout=subprocess.PIPE)
+result = subprocess.run(['qrscanner',"image.png" ], stdout=subprocess.PIPE)
 webhook = DiscordWebhook(url='', content=result.stdout.decode('utf-8'))
+webhook.execute()
+
 
 
 
